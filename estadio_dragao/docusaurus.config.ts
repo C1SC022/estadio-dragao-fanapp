03da150cf889733b05ea2c45ce7e2f2c69a55b15
LeTo <tomascostag@gmail.com>
2025-10-28 01:50:40 +0000

    changing settings

--- estadio_dragao/docusaurus.config.ts
+++ estadio_dragao/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://C1SC022.github.io',
+  url: 'https://C1SC022.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -23,8 +23,8 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'C1SC022', // Usually your GitHub org/user name.
-  projectName: 'github.io', // Usually your repo name.
-
+  projectName: 'estadio-dragao-fanapp', // Usually your repo name.
+  deploymentBranch: 'deployment',
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
